f009c6fe630f1d534e20eec78fcf72b44ce5160d
Mckenzie Melisa <melisa.mckenzie@ynov.com>
2025-03-25 16:43:10 +0100

    Added the new  publisher and publishedYear attributes.

--- src/main/java/fr/ynov/library/factory/BookFactory.java
+++ src/main/java/fr/ynov/library/factory/BookFactory.java
@@ -5,7 +5,7 @@ import fr.ynov.library.domain.Genre;
 import fr.ynov.library.domain.Language;
 
 public class BookFactory {
-    public static Book createBook(String title, String author, Genre genre, int pages, Language language) {
-        return new Book(title, author, genre, pages, language);
+    public static Book createBook(String title, String author, Genre genre, int pages, Language language, String publisher, int publishedYear) {
+        return new Book(title, author, genre, pages, language, publisher, publishedYear);
     }
 }
