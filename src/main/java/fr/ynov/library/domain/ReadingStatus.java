c39391564bfb78742f885849c48ddf696858f489
Mckenzie Melisa <melisa.mckenzie@ynov.com>
2025-03-28 16:50:36 +0100

    .

---
+++ src/main/java/fr/ynov/library/domain/ReadingStatus.java
@@ -0,0 +1,18 @@
+package fr.ynov.library.domain;
+
+public enum ReadingStatus {
+    WANT_TO_READ("Want To Read"),
+    CURRENTLY_READING("Currently Reading"),
+    READ("Read"),
+    DID_NOT_FINISH("Did Not Finish");
+
+
+    private final String displayName;
+    ReadingStatus(String displayName) {
+        this.displayName = displayName;
+    }
+    @Override
+    public String toString() {
+        return displayName;
+    }
+}
